aa7f323a9581c345548cd391c141576d643f0713
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-10-09 13:53:25 +0200

    Flush out test environment

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,4 +1,4 @@
-###
+/*
 describe "AMD support", ->
 	it "Should be AMD compatible and sub-load all requirements"
 	it "Should load ThrowProps if physics are enabled via the options object"
@@ -8,11 +8,14 @@ describe "Basic functionality", ->
 	it "Should initially set handle position if instantiated with a defined value"
 	it "Should support fluid movement along the track"
 	it "Should support stepped movement along the track"
-###
+*/
 
-describe "Memory management and disable/enable functionality", ->
-	it "Should remove all event listeners when calling the destroy method", ->
-		expect( SliderControl ).to.respondTo "destroy"
 
-	# it "Should have a disable method that allows you to temporarily turn off the slider"
-	# it "Should be able to resume from the disabled state"
+(function() {
+  describe("Memory management and disable/enable functionality", function() {
+    return it("Should remove all event listeners when calling the destroy method", function() {
+      return expect(SliderControl).to.respondTo("destroy");
+    });
+  });
+
+}).call(this);
